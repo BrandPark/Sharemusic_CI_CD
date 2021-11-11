997071237ea8e65d4dac54555719f001af64f591
brandpark <pak2005@hanmail.net>
2021-11-11 22:39:48 +0900

    알림 리스트 페이지 구현

---
+++ src/main/java/com/brandpark/sharemusic/api/v2/notification/NotificationPartialHtmlController.java
@@ -0,0 +1,62 @@
+package com.brandpark.sharemusic.api.v2.notification;
+
+import com.brandpark.sharemusic.api.v1.notification.query.NotificationQueryRepository;
+import com.brandpark.sharemusic.api.v1.notification.query.dto.NotificationInfo;
+import com.brandpark.sharemusic.api.v2.PagingHtmlCreator;
+import com.brandpark.sharemusic.api.v2.dto.PagingDto;
+import com.brandpark.sharemusic.infra.config.auth.LoginAccount;
+import com.brandpark.sharemusic.infra.config.dto.SessionAccount;
+import com.brandpark.sharemusic.modules.notification.NotificationType;
+import com.brandpark.sharemusic.modules.notification.domain.NotificationRepository;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.thymeleaf.context.Context;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class NotificationPartialHtmlController {
+
+    private final NotificationQueryRepository notificationQueryRepository;
+    private final NotificationRepository notificationRepository;
+    private final PagingHtmlCreator htmlCreator;
+
+    @GetMapping("/api/v2/notifications")
+    public  NotificationsHtmlResult getNotificationListHtml(@LoginAccount SessionAccount account
+            , @PageableDefault(size = 10) Pageable pageable
+            , @RequestParam(name = "type") String type) {
+
+        NotificationType notificationType = null;
+
+        if (!type.equals("all")) {
+             notificationType = NotificationType.valueOf(type);
+        }
+
+        PagingDto<NotificationInfo> page = notificationQueryRepository.findAllNotifications(pageable, notificationType, account.getId());
+        List<NotificationInfo> notifications = page.getContents();
+        int notReadCount = notificationRepository.countByAccountIdAndNotificationTypeAndCheckedIsFalse(account.getId(), notificationType);
+
+        Context context = new Context();
+        context.setVariable("hasNotification", notifications.size() > 0);
+        context.setVariable("notReadCount", notReadCount);
+        context.setVariable("notifications", notifications);
+
+        String notificationsHtml = htmlCreator.getListHtml("partial/notifications", context);
+
+        String paginationHtml = htmlCreator.getPaginationHtml(page);
+
+        return new NotificationsHtmlResult(notificationsHtml, paginationHtml);
+    }
+
+    @Data
+    public static class NotificationsHtmlResult {
+        final String notificationsHtml;
+        final String paginationHtml;
+    }
+}
--- src/main/java/com/brandpark/sharemusic/modules/notification/domain/NotificationRepository.java
+++ src/main/java/com/brandpark/sharemusic/modules/notification/domain/NotificationRepository.java
@@ -1,5 +1,6 @@
 package com.brandpark.sharemusic.modules.notification.domain;
 
+import com.brandpark.sharemusic.modules.notification.NotificationType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,4 +12,6 @@ public interface NotificationRepository extends JpaRepository<Notification, Long
     List<Notification> findAllFirst10ByAccountIdOrderByCreatedDateDesc(Long accountId);
 
     int countByAccountIdAndCheckedIsFalse(Long accountId);
+
+    int countByAccountIdAndNotificationTypeAndCheckedIsFalse(Long accountId, NotificationType type);
 }
