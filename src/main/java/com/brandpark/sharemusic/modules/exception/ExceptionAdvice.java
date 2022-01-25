c6de81c72dc4971fedff71cfc43e36909385c72e
brandpark <pak2005@hanmail.net>
2022-01-26 08:44:25 +0900

    aws ec 환경에서 앨범을 만들 때 MethodArgumentExcetpionHanlder가 아니라 다른 핸들러가 잡혀 주석처리

--- src/main/java/com/brandpark/sharemusic/modules/exception/ExceptionAdvice.java
+++ src/main/java/com/brandpark/sharemusic/modules/exception/ExceptionAdvice.java
@@ -2,7 +2,6 @@ package com.brandpark.sharemusic.modules.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -18,15 +17,15 @@ public class ExceptionAdvice {
         return "error/error";
     }
 
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    @ExceptionHandler(Exception.class)
-    public String exceptionHandler(Exception ex, Model model) {
-        String message = "서버에서 데이터를 처리하는 중 예기치 못한 에러가 발생했습니다.";
-
-        log.error(message);
-        log.error(ex.getMessage());
-
-        model.addAttribute("message", message);
-        return "error/error";
-    }
+//    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+//    @ExceptionHandler(Exception.class)
+//    public String exceptionHandler(Exception ex, Model model) {
+//        String message = "서버에서 데이터를 처리하는 중 예기치 못한 에러가 발생했습니다.";
+//
+//        log.error(message);
+//        log.error(ex.getMessage());
+//
+//        model.addAttribute("message", message);
+//        return "error/error";
+//    }
 }
